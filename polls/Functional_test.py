f33291337c3bbc01c1bc9150349cc762a3a52e1a
flxruizruiz <fe.ruiz@uniandes.edu.co>
2017-04-01 13:30:03 -0500

    Inicio repositorio

---
+++ polls/Functional_test.py
@@ -0,0 +1,17 @@
+__author__ = 'asistente'
+from unittest import TestCase
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+class FunctionalTest(TestCase):
+
+    def setUp(self):
+        self.browser = webdriver.Firefox()
+        self.browser.implicitly_wait(2)
+
+    def tearDown(self):
+        self.browser.quit()
+
+    def test_title(self):
+        self.browser.get('http://localhost:8000')
+        self.assertIn('Busco Ayuda', self.browser.title)
